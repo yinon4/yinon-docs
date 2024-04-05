74e14fbf51aabebf5c785c8ef98be404ac579b51
yinon <yinongilad4@gmail.com>
2024-04-05 10:26:48 +0300

    Update site URL and base URL in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'yinons/check-doc.svg',
 
   // Set the production url of your site here
-  url: 'https://yinon.docs.com',
+  url: 'https://yinon4.github.io/yinon-docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/yinon-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
