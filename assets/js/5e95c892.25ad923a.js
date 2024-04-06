59875676d0857bf31d634519784365d59e1cd2af
yinon <yinongilad4@gmail.com>
2024-04-06 19:38:07 +0300

    Updates

---
+++ assets/js/5e95c892.25ad923a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkyinon_docs=self.webpackChunkyinon_docs||[]).push([[9647],{6414:(e,s,n)=>{n.r(s),n.d(s,{default:()=>t});n(9474);var r=n(8923),c=n(5405),u=n(9724),a=n(7221),o=n(3832),d=n(3274);function t(e){return(0,d.jsx)(c.e3,{className:(0,r.A)(u.G.wrapper.docsPages),children:(0,d.jsx)(o.A,{children:(0,a.v)(e.route.routes)})})}}}]);
